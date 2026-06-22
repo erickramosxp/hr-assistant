bb66bec45506ed9ad7324a520a42cc642d4b357e
Erick Ramos <erickramossilva@gmail.com>
2026-06-22 15:34:28 -0300

    add project

---
+++ src/main/java/br/com/devsuperior/hr_assistant/HrAssistatApplication.java
@@ -0,0 +1,13 @@
+package br.com.devsuperior.hr_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrAssistatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrAssistatApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/devsuperior/hr_assistant/config/ChatClientConfig.java
@@ -0,0 +1,7 @@
+package br.com.devsuperior.hr_assistant.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatClientConfig {
+}
